d61a46a1a42f5fc57114565c00f769d74d03cb4d
EricHsia7 <54091924+EricHsia7@users.noreply.github.com>
2021-06-07 12:24:17 +0800

    Update ripple.js

--- ripple.js
+++ ripple.js
@@ -1,29 +1,59 @@
-$("a").on("click", function (eee) {
-            var ppchars = "0123456789abcdefghijklmnopqrstuvwxyz";
-            var rippleid = "";
-            for (var i = 0; i < 16; i++) {
+    function ripple(select,color,opacity,time) {
+$(select).on( "click", function( eee ) {
+  $(this).css({'width':'','height':''});
+var ogstyle = $(this).attr('style');
+
+var ppchars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            var ppid = "";
+            for (var i = 0; i < 5; i++) {
                 var randomNumber = Math.floor(Math.random() * ppchars.length);
-                rippleid += ppchars.substring(randomNumber, randomNumber + 1);
+                ppid += ppchars.substring(randomNumber, randomNumber + 1);
             }
-            var xPos = eee.pageX - $(this).offset().left - $(this).width() * 2;
-            var yPos = eee.pageY - $(this).offset().top - $(this).width() * 2;
-            $(this).append(
-                '<div style="width:' +
-                    $(this).width() * 4 +
-                    "px;height:" +
-                    $(this).width() * 4 +
-                    "px;position:absolute;top:" +
-                    yPos +
-                    "px;left:" +
-                    xPos +
-                    'px;border-radius:99em;transform:scale(0);transition:transform 0.9s,opacity 0.8s;opacity:0.3; overflow:hidden; user-select:none;" id="' +
-                    rippleid +
-                    '" class="btn-cc-pp"></div>'
-            );
-            setTimeout(function () {
-                $("#" + rippleid).css({ transform: "scale(1)", opacity: "0" });
-            }, 1);
-            setTimeout(function () {
-                $("#" + rippleid).remove();
-            }, 800);
-        });
+
+            var ppcharsa = "0123456789abcdefghijklmnopqrstuvwxyz";
+            var ppida = "";
+            for (var ia = 0; ia < 5; ia++) {
+                var randomNumbera = Math.floor(Math.random() * ppcharsa.length);
+                ppida += ppcharsa.substring(randomNumbera, randomNumbera + 1);
+            }
+$(this).attr('rippleid',ppida);
+
+  var pou = 'relative' ;
+  if($(this).css('position') === 'absolute') {
+    pou = 'absolute' ;
+  }
+  if($(this).css('position') === 'fixed') {
+    pou = 'filed' ;
+  }
+  
+            $(this).css({'width':$(this).width() + 'px','height':$(this).height() + 'px','position':pou,'overflow':'hidden','outline':'none','-webkit-tap-highlight-color':'rgba(0,0,0,0)'});
+ ggww = ($(this).width()+$(this).height())*2 ;
+if($(this).width() > $(this).height()) {
+ggww = $(this).width()*4 ;
+}
+else {
+ggww = $(this).height()*4 ;
+}
+
+ var xPos = (eee.pageX - $(this).offset().left)-(ggww/2);
+ var yPos = (eee.pageY - $(this).offset().top)-(ggww/2);
+
+$(this).append('<div style="width:' + ggww + 'px;height:' + ggww + 'px;position: absolute;top:' + yPos + 'px;left:' + xPos + 'px;border-radius:99em;transform:scale(0);transition:transform ' + (time/1000) + 's,opacity ' + (time/1.5/1000) + 's;opacity:' + opacity + ';background:' + color + '; overflow:hidden; -webkit-user-select:none; outline:none; -webkit-tap-highlight-color:rgba(0,0,0,0);transition-timing-function: ease-out;" id="' + ppid + '"></div>');
+
+setTimeout(function () {
+                    $('#' + ppid).css({'transform':'scale(1)'});
+                }, 1);
+  setTimeout(function () {
+                    $('#' + ppid).css({'opacity':'0'});
+                }, 5);
+     setTimeout(function () {
+                    $('#' + ppid).remove();
+                    $('*[rippleid="' + ppida + '"]').attr('style',ogstyle);
+                }, time/0.9);
+  if($(this).attr('ripple-onclick') != '') {
+
+  eval('setTimeout(function() {' + $(this).attr('ripple-onclick') + '},' + ((5+(time/1.5))/2) + ');');
+  
+}
+});
+}
